933c13c8f67bc31d95efa203701d53e74adb1d30
DennisFriedl <DennisFriedl@paderborn.com>
2018-07-28 15:53:27 +0200

    Icons bei Boxen und beim Git-Verweis
    Durch Awesome Fonts

--- Website/js/urkunden.js
+++ Website/js/urkunden.js
@@ -113,3 +113,11 @@ function zuweisung() {
     $('tei-name').attr('data-toggle', 'popover');
     $('[data-toggle="popover"]').popover();
 };
+
+$(document).ready(function (){
+    $('.collapse').on('shown.bs.collapse', function (){
+        $(this).parent().find('.fa-plus-circle').removeClass('fa-plus-circle').addClass('fa-minus-circle')
+    }).on('hidden.bs.collapse', function (){
+        $(this).parent().find('.fa-minus-circle').removeClass('fa-minus-circle').addClass('fa-plus-circle')
+    })
+});
